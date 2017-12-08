d13e8984623666a1beef7923704fe49ea833f64e
Suchali <suchali93@hotmail.com>
2017-12-07 22:17:43 -0500

    portfolio changed to project

--- js/freelancer.js
+++ js/freelancer.js
@@ -50,13 +50,13 @@
   $(window).scroll(navbarCollapse);
 
   // Modal popup$(function () {
-  $('.portfolio-item').magnificPopup({
+  $('.projects-item').magnificPopup({
     type: 'inline',
     preloader: false,
     focus: '#username',
     modal: true
   });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  $(document).on('click', '.projects-modal-dismiss', function(e) {
     e.preventDefault();
     $.magnificPopup.close();
   });
