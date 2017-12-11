06769550c0f87b98b5562e103b901db5a74740b1
Suchali <suchali93@hotmail.com>
2017-12-10 19:43:11 -0500

    project pictures updated

--- js/freelancer.js
+++ js/freelancer.js
@@ -84,7 +84,6 @@
               $("#sendMessageButton").attr("disabled", true);
               $("#sendMessageButton").addClass("disabled");
               $('#success').html("<div class='alert alert-success'>");
-              $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
               $('#success > .alert-success').append("<strong>Thank you, " + name + "! Your message has been sent. I will be in touch soon.</strong>");
               $('#success > .alert-success').append('</div>');
           }
